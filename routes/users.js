7fca0f0446debce21e29fc937d6091eb24f3651c
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-03-24 11:43:54 -0500

    Modified the users endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+let count = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+ count +=1; 
+ res.send(`User accesses are: ${count}`)
 });
 
 module.exports = router;
