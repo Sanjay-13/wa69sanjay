276aa944ae330ec9612552b002a5d927b54b1798
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-03-24 13:10:09 -0500

    added the Math function

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 //let count = 0;
-let x = -10
+let x = -10;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next){
  //count +=1; 
  //res.send(`User accesses are: ${count}`)
- req.send(`Math.abs applied to ${x} is  ${Math.abs(x)}`)
+ req.send(`Math.abs applied to is  ${Math.abs(x)}`)
 });
 
 module.exports = router;
