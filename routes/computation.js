dc3eebbf108a785975b781af659c3d4e6d8070f5
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-03-24 13:41:11 -0500

    added the Math function

--- routes/computation.js
+++ routes/computation.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('computation', { title: 'Math Function' });
+    value = Math.floor(Math.random() * Math.floor(15));
+    if(req.query.value){
+        value = req.query.value
+    }
+  res.send(`Math.abs function applied to ${value} is ${Math.abs(value)}`);
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-//let count = 0;
-let x = -10;
+let count = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next){
- //count +=1; 
- //res.send(`User accesses are: ${count}`)
- req.send(`Math.abs applied to is  ${Math.abs(x)}`)
+ count +=1; 
+ res.send(`User accesses are: ${count}`)
+
 });
 
 module.exports = router;
